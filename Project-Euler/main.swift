d6967a0234e24d892809acbb06fa5f22cf169a5d
Morshed <morshedalamustc22@gmail.com>
2020-04-02 11:37:33 +0600

    Initial Commit

---
+++ Project-Euler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Project-Euler
+//
+//  Created by Morshed Alam on 2/4/20.
+//  Copyright © 2020 Morshed Alam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
