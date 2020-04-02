530212f8abf8b57f72961cd1759763a20b3ad72f
Morshed <morshedalamustc22@gmail.com>
2020-04-02 12:06:42 +0600

    First problem solved

---
+++ Project-Euler/01.swift
@@ -0,0 +1,11 @@
+//
+//  01.swift
+//  Project-Euler
+//
+//  Created by Morshed Alam on 2/4/20.
+//  Copyright © 2020 Morshed Alam. All rights reserved.
+//
+
+import Foundation
+
+
--- Project-Euler/main.swift
+++ Project-Euler/main.swift
@@ -9,4 +9,10 @@
 import Foundation
 
 print("Hello, World!")
-
+var container = [Int]()
+for i in 0..<1000{
+    if i%3 == 0 || i%5 == 0 {
+        container.append(i)
+    }
+}
+print(container.reduce(0, +))
